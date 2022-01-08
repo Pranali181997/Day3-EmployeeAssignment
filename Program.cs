b16a66e066b81a61a8a92b998d4e8f673ccf423b
Pranali <pranalilambat00@gmail.com>
2022-01-08 21:57:12 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+            //Employee.DailyWages();
+            //UC2.DailyWages();
+            //Uc3.PartTime();
+            //UC4switch.SwitchCase();
+            //UC5.MonthlySalary();
+            //UC6.maxRateInMonth();
+        }
+    }
+}
