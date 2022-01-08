c142fe3e08132bd2370890d27658de7ae5199e9a
Pranali <96486431+Pranali181997@users.noreply.github.com>
2022-01-08 22:04:59 +0530

    Add files via upload

---
+++ UC2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee
+{
+    class UC2
+    {
+        public static void DailyWages()
+        {
+            Random random = new Random();
+            int number = random.Next(2);
+            {
+                if (number == 0)
+                {
+                    int WagePerHr = 20;
+                    int FullDayHr = 8;
+                    int DailyWages = WagePerHr *= FullDayHr;
+                    {
+                        Console.WriteLine("Daily Employee Wages are=" + DailyWages);
+                    }
+                }
+                else
+                {
+                    int DailyWages = 0;
+                    Console.WriteLine("Daily Employee Wages are=" + DailyWages);
+                }
+                }
+            }
+        }
+    }
